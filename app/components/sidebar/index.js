0755a1e6371cc919be903a39b5697404185b06e7
pradiktabagus <pradiktabagus@gmail.com>
2021-06-18 17:39:44 +0700

    bot

--- app/components/sidebar/index.js
+++ app/components/sidebar/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { fagith } from '@fortawesome/free-solid-svg-icons';
+import Linkedin from '../../images/linkedin.svg';
+import Github from '../../images/github.svg';
+import Mail from '../../images/envelope.svg';
+import Instagram from '../../images/instagram.svg';
+import Wattpad from '../../images/wattpad.svg';
 import 'al-styles/components/sidebar.scss';
 function index(props) {
   return (
@@ -12,8 +15,52 @@ function index(props) {
           <label>Rofiyanto Bagus P.</label>
         </div>
         <div className="find-me">
-          <ul>
-            <li>Linkend</li>
+          <ul className="social-media">
+            <li className="items">
+              <a
+                href="https://www.linkedin.com/in/rofiyanto-bagus-614a09a9/"
+                target="_blank"
+              >
+                <img src={Linkedin} alt="Linkedin" />
+                <h4>Rofiyanto</h4>
+              </a>
+            </li>
+            <li className="items">
+              <a
+                href="https://www.linkedin.com/in/rofiyanto-bagus-614a09a9/"
+                target="_blank"
+              >
+                <img src={Github} alt="Linkedin" />
+                <h4>pradiktabagus</h4>
+              </a>
+            </li>
+            <li className="items">
+              <a
+                href="https://www.linkedin.com/in/rofiyanto-bagus-614a09a9/"
+                target="_blank"
+              >
+                <img src={Instagram} alt="Linkedin" />
+                <h4>rofiyanto_bp</h4>
+              </a>
+            </li>
+            <li className="items">
+              <a
+                href="https://www.linkedin.com/in/rofiyanto-bagus-614a09a9/"
+                target="_blank"
+              >
+                <img src={Mail} alt="Linkedin" />
+                <h4>pradiktabagus@gmail.com</h4>
+              </a>
+            </li>
+            <li className="items">
+              <a
+                href="https://www.linkedin.com/in/rofiyanto-bagus-614a09a9/"
+                target="_blank"
+              >
+                <img src={Wattpad} alt="Linkedin" />
+                <h4>mika_isuzu</h4>
+              </a>
+            </li>
           </ul>
         </div>
       </div>
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,7 +1,127 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from 'al-comps/sidebar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faKeyboard } from '@fortawesome/free-solid-svg-icons';
 import 'al-styles/base.scss';
-export default function App() {
+export default function App(props) {
+  const [state, setState] = useState({
+    typing: null,
+    chat: [],
+  });
+
+  const trigger = [
+    ['whats your name', 'your name', 'hi what your name', 'who are you'],
+    ['hi', 'hey', 'hello'],
+    ['how are you', 'how are things'],
+    ['what is going on', 'what is up'],
+    ['happy', 'good', 'well', 'fantastic', 'cool'],
+    ['bad', 'bored', 'tired', 'sad'],
+    ['tell me story', 'tell me joke'],
+    ['thanks', 'thank you'],
+    ['bye', 'good bye', 'goodbye'],
+  ];
+  const reply = [
+    ['im Rofi'],
+    ['Hello!', 'Hi!', 'Hey!', 'Hi there!'],
+    [
+      'Fine... how are you?',
+      'Pretty well, how are you?',
+      'Fantastic, how are you?',
+    ],
+    ['Nothing much', 'Exciting things!'],
+    ['Glad to hear it'],
+    ['Why?', 'Cheer up buddy'],
+    ['What about?', 'Once upon a time...'],
+    ["You're welcome", 'No problem'],
+    ['Goodbye', 'See you later'],
+  ];
+  const resetState = () => {
+    setState({
+      typing: null,
+      chat: [],
+    });
+    document.getElementById('quetion').value = null;
+  };
+
+  function compare(triggerArray, replyArray, text) {
+    let item;
+    let items;
+    for (let x = 0; x < triggerArray.length; x++) {
+      for (let y = 0; y < replyArray.length; y++) {
+        if (triggerArray[x][y] == text) {
+          items = replyArray[x];
+          item = items[Math.floor(Math.random() * items.length)];
+        }
+      }
+    }
+    return item;
+  }
+
+  function output(input) {
+    let product;
+    let text = input.toLowerCase().replace(/[^\w\s\d]/gi, '');
+    text = text
+      .replace(/ a /g, ' ')
+      .replace(/i feel /g, '')
+      .replace(/whats/g, 'what is')
+      .replace(/please /g, '')
+      .replace(/ please/g, '');
+
+    if (compare(trigger, reply, text)) {
+      product = compare(trigger, reply, text);
+    } else {
+      product = "sorry I haven't learned it, another quetion please!";
+    }
+    const { chat } = state;
+    const outputChat = {
+      type: 'incoming',
+      message: product,
+      id: Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1),
+    };
+    const newData = chat;
+    newData.push(outputChat);
+    setState(prevState => ({
+      ...prevState,
+      chat: [...newData],
+    }));
+  }
+
+  const handleSend = e => {
+    e.preventDefault();
+    const { chat } = state;
+    if (state.typing.toLowerCase() === '/clear') {
+      resetState();
+      return;
+    }
+    const newChat = {
+      type: 'outgoing',
+      message: state.typing,
+      id: Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1),
+    };
+    const newData = chat;
+    newData.push(newChat);
+    setState(prevState => ({
+      ...prevState,
+      chat: [...newData],
+      typing: null,
+    }));
+    setTimeout(() => {
+      output(newChat.message);
+    }, 3000);
+    document.getElementById('quetion').value = null;
+  };
+
+  const handleTypingChange = e => {
+    const { value } = e.target;
+    setState(prevState => ({
+      ...prevState,
+      typing: value,
+    }));
+  };
   return (
     <div className="root">
       <div className="section-app">
@@ -14,30 +134,28 @@ export default function App() {
                   <div className="avatar" />
                 </div>
               </div>
-              <div className="incoming">
-                <div className="bubble">Hey, Father's Day is coming up..</div>
-                <div className="bubble">
-                  What are you getting.. Oh, oops sorry dude.
+              {state.chat.map(ch => (
+                <div className={ch.type}>
+                  <div className="bubble">{ch.message}</div>
                 </div>
-              </div>
-              <div className="outgoing">
-                <div className="bubble lower">Nah, it's cool.</div>
-                <div className="bubble">
-                  Well you should get your Dad a cologne. Here smell it. Oh
-                  wait! ...
-                </div>
-              </div>
+              ))}
             </div>
             <div className="section-footer">
-              <div className="section-quetion">
-                <textarea
+              <form className="section-quetion" onSubmit={handleSend}>
+                <input
+                  id="quetion"
+                  value={state.typing}
+                  name="outgoing"
+                  onChange={handleTypingChange}
                   className="quetion-area"
                   placeholder="Ask anything about me...!"
                 />
                 <div className="btn-area">
-                  <button className="btn-send">send</button>
+                  <button className="btn-send" type="submit">
+                    <FontAwesomeIcon icon={faKeyboard} />
+                  </button>
                 </div>
-              </div>
+              </form>
             </div>
           </div>
         </div>
