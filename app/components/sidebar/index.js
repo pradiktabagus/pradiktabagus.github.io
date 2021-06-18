cd1f4342ab7c2cc39a88ff819b0649358cf0e62e
pradiktabagus <pradiktabagus@gmail.com>
2021-06-18 18:00:25 +0700

    position

--- app/components/sidebar/index.js
+++ app/components/sidebar/index.js
@@ -14,6 +14,14 @@ function index(props) {
           <div className="photo-profile" />
           <label>Rofiyanto Bagus P.</label>
         </div>
+        <select name="position" id="position" className="position">
+          <option key={1} value={1}>
+            Frontend Developer
+          </option>
+          <option key={2} value={2}>
+            Android Developer
+          </option>
+        </select>
         <div className="find-me">
           <ul className="social-media">
             <li className="items">
@@ -26,37 +34,25 @@ function index(props) {
               </a>
             </li>
             <li className="items">
-              <a
-                href="https://www.linkedin.com/in/rofiyanto-bagus-614a09a9/"
-                target="_blank"
-              >
-                <img src={Github} alt="Linkedin" />
+              <a href="https://github.com/pradiktabagus" target="_blank">
+                <img src={Github} alt="Github" />
                 <h4>pradiktabagus</h4>
               </a>
             </li>
             <li className="items">
-              <a
-                href="https://www.linkedin.com/in/rofiyanto-bagus-614a09a9/"
-                target="_blank"
-              >
-                <img src={Instagram} alt="Linkedin" />
+              <a href="https://www.instagram.com/rofiyanto_bp/" target="_blank">
+                <img src={Instagram} alt="Instagram" />
                 <h4>rofiyanto_bp</h4>
               </a>
             </li>
             <li className="items">
-              <a
-                href="https://www.linkedin.com/in/rofiyanto-bagus-614a09a9/"
-                target="_blank"
-              >
+              <a href="#" target="_blank">
                 <img src={Mail} alt="Linkedin" />
                 <h4>pradiktabagus@gmail.com</h4>
               </a>
             </li>
             <li className="items">
-              <a
-                href="https://www.linkedin.com/in/rofiyanto-bagus-614a09a9/"
-                target="_blank"
-              >
+              <a href="https://www.wattpad.com/user/mika_Isuzu" target="_blank">
                 <img src={Wattpad} alt="Linkedin" />
                 <h4>mika_isuzu</h4>
               </a>
